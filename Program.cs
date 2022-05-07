ab69793e864cc886f48b9e3706945a78c456428f
adassimo25 <adasimbad99@gmail.com>
2022-05-07 18:19:00 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCTS_Research
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
