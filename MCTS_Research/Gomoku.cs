466bb83a960794da4daceff261b25b741db08b8c
adassimo25 <adasimbad99@gmail.com>
2022-05-08 18:19:49 +0200

    Init WinForms project

---
+++ MCTS_Research/Gomoku.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCTS_Research
+{
+    public partial class Gomoku : Form
+    {
+        public Gomoku()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
