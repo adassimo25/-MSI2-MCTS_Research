f5d97814f765f005d12b845e51605a83f2d5bf82
adassimo25 <adasimbad99@gmail.com>
2022-06-11 18:35:14 +0200

    Cleanup

--- GomokuGUI/Gomoku.cs
+++ GomokuGUI/Gomoku.cs
@@ -1,12 +1,4 @@
-﻿using GomokuGUI;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace GomokuGUI
@@ -46,22 +38,18 @@ namespace GomokuGUI
 
         private void RadioButtonBvsB_CheckedChanged(object sender, EventArgs e)
         {
-
         }
 
         private void RadioButtonBvsP_CheckedChanged(object sender, EventArgs e)
         {
-
         }
 
         private void RadioButtonPvsP_CheckedChanged(object sender, EventArgs e)
         {
-
         }
 
         private void ButtonStart_Click(object sender, EventArgs e)
         {
-
         }
     }
 }
