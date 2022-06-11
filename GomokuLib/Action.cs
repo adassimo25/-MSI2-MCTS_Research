0ad99c5d95a92cf4caa6cbeb710c82949e08d86e
Piotr Kryczka <72410501+piotrek999@users.noreply.github.com>
2022-06-11 17:59:16 +0200

    Create skeleton

---
+++ GomokuLib/Action.cs
@@ -0,0 +1,14 @@
+﻿namespace GomokuLib
+{
+    public class Action
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Action(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
